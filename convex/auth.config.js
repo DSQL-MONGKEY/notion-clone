ad79dc6a2ad9f77dbf08a09e514ae0a3de41daef
Dp_ras <51804858+DSQL-MONGKEY@users.noreply.github.com>
2023-11-02 21:12:58 +0700

    chore<anonymous import>: disable no anonymous import

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,8 +1,9 @@
+/* eslint-disable import/no-anonymous-default-export */
 export default {
    providers: [
       {
-        domain: "https://beloved-hound-95.clerk.accounts.dev",
-        applicationID: "convex",
+         domain: "https://beloved-hound-95.clerk.accounts.dev",
+         applicationID: "convex",
       },
-    ]
+   ]
 }
