dd68d82ad8c43096a25132cf5b7b684186256525
Dp_ras <51804858+DSQL-MONGKEY@users.noreply.github.com>
2023-11-03 16:44:45 +0700

    chore<>: remove blank space and fix typography

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,23 +1,46 @@
 "use client"
 
+import Link from "next/link";
+import { Spinner } from "@/components/spinner";
 import { Button } from "@/components/ui/button";
-import { ArrowBottomRightIcon, ArrowRightIcon } from "@radix-ui/react-icons";
+import { ArrowRightIcon } from "@radix-ui/react-icons";
+import { useConvexAuth } from "convex/react";
+import { SignInButton } from "@clerk/clerk-react";
 
 const Heading = () => {
+   const {isAuthenticated, isLoading} = useConvexAuth()
+
    return (
       <div className="max-w-3xl space-y-4">
          <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
-            Your Ideas, Documents, & Plans. Unified. Welcome to 
+            Your Ideas, Documents, & Plans. Unified. Welcome to &nbsp; 
             <span className="underline">Jotion</span>
          </h1>
          <h3 className="text-base sm:text-xl md:text-2xl font-medium">
             Jotion is the connected workspace where <br />
             better, faster work happens.
          </h3>
-         <Button>
-            Enter Jotion &nbsp;
-            <ArrowRightIcon/>
-         </Button>
+         {isLoading && (
+            <div className="w-full flex items-center justify-center">
+               <Spinner size="lg"/>
+            </div>
+         )}
+         {isAuthenticated && !isLoading && (
+            <Button asChild>
+               <Link href="/documents">
+                  Enter Jotion &nbsp;
+                  <ArrowRightIcon/>
+               </Link>
+            </Button>
+         )}
+         {!isAuthenticated && !isLoading && (
+            <SignInButton mode="modal">
+               <Button>
+                  Get Jotion free &nbsp;
+                  <ArrowRightIcon/>
+               </Button>
+            </SignInButton>
+         )}
       </div>
    );
 }
